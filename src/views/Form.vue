ea291b9b628e20af60a506cf1fb087e95c588120
Edwin J. Páez <edwinjpr11@gmail.com>
2023-03-21 16:15:52 -0500

    feat(project): create initial setup

---
+++ src/views/Form.vue
@@ -0,0 +1,21 @@
+<template>
+  <Button @test-test="changeSpanData" label="Soy una prop" color="red" backgroundColor="white" padding="20px"
+    font="small"></Button>
+  <span>{{ contador.count }}</span>
+  <Button @test-test="changeSpanData" label="Soy una prop" color="blue" backgroundColor="white" padding="20px"
+    font="small"></Button>
+  <span>{{ contador.doubleCount }}</span>
+</template>
+
+<script setup>
+import Button from '../components/Button.vue';
+
+import { useCounterStore } from '@/stores/counter';
+
+const contador = useCounterStore();
+
+
+function changeSpanData(count) {
+  console.log(count);
+}
+</script>
